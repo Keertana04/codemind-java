71cce7badeccdf44ec6845618cd3b9b0ac69573b
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2023-08-04 21:14:52 +0530

    import java.util.Scanner; public class Multiply{     public static void main(String args[]){         int n1,n2,m;         Scanner sc = new Scanner(System.in);         n1 = sc.nextInt();         n2 = sc.nextInt();         sc.close();         m = n1 * n2;         System.out.println(+m);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Multiply{
+    public static void main(String args[]){
+        int n1,n2,m;
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        sc.close();
+        m = n1 * n2;
+        System.out.println(+m);
+    }
+}
