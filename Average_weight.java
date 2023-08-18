7a6888fb86a3c65b36bcd7846f7449700b76a7e3
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2023-08-18 16:19:26 +0530

    import java.util.*; public class avg{ public static void main(String[] args){     Scanner sc = new Scanner (System.in);     int x = sc.nextInt();     int a = sc.nextInt();     int b = sc.nextInt();     sc.close();     int c = 3*x-a-b;     System.out.println(c); } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class avg{
+public static void main(String[] args){
+    Scanner sc = new Scanner (System.in);
+    int x = sc.nextInt();
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    sc.close();
+    int c = 3*x-a-b;
+    System.out.println(c);
+}
+}
